5acc7665bf9b8ce09f04dd5980c9c6de76670bf2
Jiaheng Tang <jiaheng@ucsb.edu>
2018-05-15 17:07:40 -0700

    changed some name

--- app/src/main/java/com/example/jiaheng/rydebot/WelcomeActivity.java
+++ app/src/main/java/com/example/jiaheng/rydebot/WelcomeActivity.java
@@ -80,20 +80,13 @@ public class WelcomeActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_forum) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_chat) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_about) {
 
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
